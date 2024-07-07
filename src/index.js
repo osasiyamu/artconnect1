7ae5824f80e123a39f373e1b9648e6f42d8546e7
osasiyamu <140278554+osasiyamu@users.noreply.github.com>
2024-07-07 13:33:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './Navbar';
-import { BrowserRouter as Router } from 'react-router-dom/cjs/react-router-dom.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
